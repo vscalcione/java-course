29b20b43eceb54dd03a48e21b4bf263a4dad3237
vscalcione <vincenzo99.scalcione@gmail.com>
2020-01-28 19:24:09 +0100

    Reverted classes of polygon exercises

--- src/main/java/it/intersistemi/corsojava/polimorfism/exercises/polygons/GenericPolygon.java
+++
@@ -1,8 +0,0 @@
-package it.intersistemi.corsojava.polimorfism.exercises.polygons;
-
-public interface GenericPolygon {
-
-	public int getSidesNumber();
-	double calculatePerimeter();
-	double calculateArea();
-}
--- src/main/java/it/intersistemi/corsojava/polimorfism/exercises/polygons/Start.java
+++
@@ -1,7 +0,0 @@
-package it.intersistemi.corsojava.polimorfism.exercises.polygons;
-
-public class Start {
-	public static void main(String[] args) {
-		
-	}
-}
